03f1300e2a08be4149cf4afef46e235caafcf17e
devb0x <moniezgeoffrey59@gmail.com>
2023-09-29 09:25:34 +0200

    add basic html and fetch function

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-console.log('hello friend')
+async function fetchWeather()  {
+	const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London,Uk&APPID=32a0164c9773164342f1cd2352e0510a')
+	const data = await response.json()
+
+	if (response.ok === true) {
+		return console.log(data.name)
+	}
+	throw new Error(response.error.message)
+}
+
+fetchWeather()
 
